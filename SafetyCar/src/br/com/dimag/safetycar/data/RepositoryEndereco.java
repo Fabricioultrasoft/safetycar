669f6c71a4e8d9e0e28c02bb8ba2bf13fa045fc0
mariobruno1 <mariobruno1@81c78f6c-9cad-11dd-93bb-3f8bb607bf45>
2008-12-02 00:46:09 +0000

    Repositorios e cadastros completados segundo baseEntity
    Lembrar de verificar os campos que se repetem em cliente e pessoa.
    
    Mário e Augusto

---
+++ SafetyCar/src/br/com/dimag/safetycar/data/RepositoryEndereco.java
@@ -0,0 +1,52 @@
+package br.com.dimag.safetycar.data;
+
+import java.util.List;
+
+import br.com.dimag.safetycar.data.api.IRepositoryEndereco;
+import br.com.dimag.safetycar.data.transaction.HibernateInterceptorAnnotation;
+import br.com.dimag.safetycar.data.transaction.HibernateTransaction;
+import br.com.dimag.safetycar.data.transaction.HibernateUtil;
+import br.com.dimag.safetycar.data.transaction.TransactionClass;
+import br.com.dimag.safetycar.model.Endereco;
+
+public class RepositoryEndereco implements IRepositoryEndereco {
+
+	private static RepositoryEndereco instance;	
+	
+	private Class<Endereco> clazz = Endereco.class;
+	
+	public static RepositoryEndereco getInstance() throws Exception {
+		if (instance == null) {
+			instance = (RepositoryEndereco) TransactionClass.create(
+					RepositoryEndereco.class,
+					HibernateInterceptorAnnotation.class);
+		}
+		return instance;
+	}
+
+	@Override
+	@HibernateTransaction
+	public void delete(Endereco endereco) {
+		HibernateUtil.getSession().delete(endereco);
+	}
+
+	@Override
+	@HibernateTransaction
+	public void insert(Endereco endereco) {
+		HibernateUtil.getSession().save(endereco);
+	}
+
+	@Override
+	@HibernateTransaction
+	public void update(Endereco endereco) {
+		HibernateUtil.getSession().merge(endereco);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@HibernateTransaction
+	public List<Endereco> list() {
+		return HibernateUtil.getSession().createQuery(
+				"from " + clazz.getSimpleName()).list();
+	}
+}
---
+++ SafetyCar/src/br/com/dimag/safetycar/data/RepositoryFuncionario.java
@@ -0,0 +1,52 @@
+package br.com.dimag.safetycar.data;
+
+import java.util.List;
+
+import br.com.dimag.safetycar.data.api.IRepositoryFuncionario;
+import br.com.dimag.safetycar.data.transaction.HibernateInterceptorAnnotation;
+import br.com.dimag.safetycar.data.transaction.HibernateTransaction;
+import br.com.dimag.safetycar.data.transaction.HibernateUtil;
+import br.com.dimag.safetycar.data.transaction.TransactionClass;
+import br.com.dimag.safetycar.model.Funcionario;
+
+public class RepositoryFuncionario implements IRepositoryFuncionario {
+
+	private static RepositoryFuncionario instance;	
+	
+	private Class<Funcionario> clazz = Funcionario.class;
+	
+	public static RepositoryFuncionario getInstance() throws Exception {
+		if (instance == null) {
+			instance = (RepositoryFuncionario) TransactionClass.create(
+					RepositoryFuncionario.class,
+					HibernateInterceptorAnnotation.class);
+		}
+		return instance;
+	}
+
+	@Override
+	@HibernateTransaction
+	public void delete(Funcionario funcionario) {
+		HibernateUtil.getSession().delete(funcionario);
+	}
+
+	@Override
+	@HibernateTransaction
+	public void insert(Funcionario funcionario) {
+		HibernateUtil.getSession().save(funcionario);
+	}
+
+	@Override
+	@HibernateTransaction
+	public void update(Funcionario funcionario) {
+		HibernateUtil.getSession().merge(funcionario);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@HibernateTransaction
+	public List<Funcionario> list() {
+		return HibernateUtil.getSession().createQuery(
+				"from " + clazz.getSimpleName()).list();
+	}
+}
