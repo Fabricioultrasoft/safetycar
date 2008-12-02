9327ff864d8fcfc56c5ee061fd6e92ddfbb38311
mariobruno1 <mariobruno1@81c78f6c-9cad-11dd-93bb-3f8bb607bf45>
2008-12-02 00:46:09 +0000

    Repositorios e cadastros completados segundo baseEntity
    Lembrar de verificar os campos que se repetem em cliente e pessoa.
    
    Mário e Augusto

---
+++ trunk/SafetyCar/src/br/com/dimag/safetycar/data/api/IRepositoryTipoFuncionario.java
@@ -0,0 +1,7 @@
+package br.com.dimag.safetycar.data.api;
+
+import br.com.dimag.safetycar.model.TipoFuncionario;
+
+public interface IRepositoryTipoFuncionario extends IRepository<TipoFuncionario> {
+
+}
